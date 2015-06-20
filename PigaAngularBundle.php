5dcd7282018abe11a9e3d65a6b5e9b16231a43df
Piotr <galaspiotrek@gmail.com>
2015-06-20 12:59:49 +0200

    initial commit with auto generated bundle

---
+++ PigaAngularBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Piga\AngularBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PigaAngularBundle extends Bundle
+{
+}
---
+++ Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Piga\AngularBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+
+}
